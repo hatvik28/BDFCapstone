8730e5605fe0a3a78e8b133d916dbe9f9d0e8f16
hatvik28 <143751098+hatvik28@users.noreply.github.com>
2025-05-05 15:07:25 -0400

    Update __init__.py

--- spotbugs1/app/routes/__init__.py
+++ spotbugs1/app/routes/__init__.py
@@ -8,6 +8,5 @@ blueprints = [api_bp]
 
 
 def register_routes(app):
-    """Register all blueprints (route handlers) with the Flask app."""
     for bp in blueprints:
         app.register_blueprint(bp)
